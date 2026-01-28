54360918636c7e4917bf2197e2156019736d9b7e
rohithreddy-m <mallidisairohithreddy@gmail.com>
2026-01-28 23:25:36 +0530

    Delete 3.vowelscount.js

--- 3.vowelscount.js
+++
@@ -1,8 +0,0 @@
-const input="aeiou AEIOU RHTHRDDY"
-// for (let i =0; i<input.length;i++){
-for (let i of input){
-    // if ("aeiou ".includes(input[i]) || "AEIOU".includes(input[i])){
-    if ("aeiou ".includes(i) || "AEIOU".includes(i)){
-        console.log(i)
-    }
-}
