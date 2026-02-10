10cb7294057159537f11aa26d9efcdbb7464ae64
rohith <mallidisairohithreddy@gmail.com>
2026-02-10 21:16:57 +0530

    All is Done

---
+++ 4.Arrays/7.Rotateleft.js
@@ -0,0 +1,23 @@
+const input = [9, 8, 7, 6, 5, 4];
+// 0-3
+// 1-4
+// 2-0
+// 3-1
+// 4-2
+const rotate = 3;
+const ans=[]
+for (let i = rotate ; i < input.length;i++) {
+    ans.push(input[i])
+} ans.push(...input.slice(0,rotate))
+console.log(ans)
+const res = [];
+for (let i = 0; i < input.length; i++) {
+  if (i < rotate) {
+    console.log(res)
+    res[input.length - rotate + i] = input[i];
+  } else {
+    console.log(res)
+    res[i - rotate] = input[i];
+  }
+}
+console.log(res);
