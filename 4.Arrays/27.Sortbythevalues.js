f91d33a44112d2090dbc693749e2b2cd290472f9
rohith <mallidisairohithreddy@gmail.com>
2026-02-13 01:03:41 +0530

    All is Done

--- 4.Arrays/27.Sortbythevalues.js
+++ 4.Arrays/27.Sortbythevalues.js
@@ -1,22 +1,22 @@
-const input={"a":2,"n":9,"u":1}
+const input={"a":5,"n":2,"u":-1}
 let list=[]
 for (let i in input) {
     list.push([i,input[i]])
 }
 let temp
- for (let j in list) {
-    for (let k in list) {
-        if (list[j][1] < list[k][1]) {
+ for (let j=0;j<list.length;j++) {
+    for (let k=j;k<list.length;k++) {
+        if (list[j][1] > list[k][1]) {
             temp=list[k]
             list[k]=list[j]
             list[j]=temp
-            temp=list[k]
         }
     }
-} let obj={}
+}
+ let obj={}
 let key
 for (let g of list) {
     key=g[0]
     obj[key]=g[1]
+    console.log(obj)
 }
-console.log(obj)
