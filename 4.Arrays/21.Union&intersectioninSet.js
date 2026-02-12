0ce1e2c1eee9845281b118ba08a173ac01247008
rohith <mallidisairohithreddy@gmail.com>
2026-02-12 17:11:34 +0530

    All is Done

---
+++ 4.Arrays/21.Union&intersectioninSet.js
@@ -0,0 +1,4 @@
+const inputOne=new Set([1,2,3,4,5,6,4])
+const inputTwo=new Set([1,2,3,4,9,7,6,4])
+console.log(...inputOne.intersection(inputTwo))
+console.log(...inputOne.union(inputTwo))
---
+++ 4.Arrays/22.RemoveduplicatesstringinSet.js
@@ -0,0 +1,4 @@
+const input="asdfasd"
+const inputOne=new Set (input)
+console.log([inputOne])
+console.log([...inputOne].join(''))
