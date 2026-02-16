53e3b48173fd1e5f8405d14462026586558643f4
rohith <mallidisairohithreddy@gmail.com>
2026-02-16 15:31:46 +0530

    All is Done

---
+++ 5.Function/9.PerfectNumber.js
@@ -0,0 +1,14 @@
+function PerfectNumber(input) {
+    let ans=0
+    let half=Math.ceil(input/2)
+    for (let i=1;ans<=half;i++) {
+        if (input % i ===0) {
+            ans+=i
+        }         
+    } if (ans==input) {
+        console.log(`${input} is PerfectNumber.`)
+    } else {
+        console.log(`${input} is Not PerfectNumbet.`)
+    }
+}
+PerfectNumber(28)
